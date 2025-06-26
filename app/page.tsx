4c8a0c4b220c170915fab2e269e4ed21b81654f3
David Lee <Dai180293@gmail.com>
2025-06-26 13:08:55 +0100

    added landing section text content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import SocialLinks from "@/components/SocialLinks";
+
 export default function Home() {
     return (
-        <>
-            main content
-        </>
+        <section>
+            <section className="py-10 px-5 md:py-16 md:px-10 lg:py-20 lg:px-20 space-y-20">
+                <article className="space-y-5 max-w-2xl">
+                    <h1 className="text-2xl lg:text-4xl max-w-sm lg:max-w-xl font-serif">
+                        A Frontend Developer & Kickboxer based in the UK.
+                    </h1>
+                    <p>
+                        I&apos;m a React-based Dev that aims to create an experience that goes
+                        beyond the visuals—sites that serve, inform, and engage. I build with
+                        intention, not just for clicks or profit, but to make a genuine impact.
+                    </p>
+                </article>
+                <SocialLinks />
+            </section>
+        </section>
     );
 }
