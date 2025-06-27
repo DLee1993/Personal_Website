7f98da30ec3b8bc92648bc1f4c1b0b873bf0d48a
David Lee <Dai180293@gmail.com>
2025-06-27 10:12:52 +0100

    added fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { Bebas_Neue, Nunito } from "next/font/google";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import "./globals.css";
 
 export const metadata: Metadata = {
     title: "David Lee | Frontend Developer",
@@ -18,6 +20,17 @@ export const metadata: Metadata = {
     },
 };
 
+const bebas = Bebas_Neue({
+    subsets: ["latin"],
+    weight: "400",
+    variable: "--font-bebas",
+});
+const nunito = Nunito({
+    subsets: ["latin"],
+    weight: ["200", "300", "400", "500", "600", "700", "800", "900", "1000"],
+    variable: "--font-nunito",
+});
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -25,9 +38,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className="min-h-screen antialiased bg-primary text-secondary">
+            <body className={`min-h-screen antialiased ${nunito.variable} ${bebas.variable}`}>
                 <Header />
                 <main>{children}</main>
+                <Footer />
             </body>
         </html>
     );
