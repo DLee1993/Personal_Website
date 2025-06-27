b975888d46299aae30171a7d07c86cacaa6e47eb
David Lee <Dai180293@gmail.com>
2025-06-27 10:13:09 +0100

    removed all content

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -1,12 +1,7 @@
-import data from "@/data/projectList.json";
-import Project from "@/components/Project";
-
 export default function ProjectsList() {
     return (
-        <section className="px-2 h-fit relative grid grid-cols-2 place-items-center divide-tertiary/50 divide-x mb-40">
-            {data.map((project, i) => (
-                <Project project={project} index={i} key={i} />
-            ))}
+        <section>
+            project list
         </section>
     );
 }
