2f114fd4acec17bb24496e2999fe9cbda3d1c717
David Lee <Dai180293@gmail.com>
2025-06-29 13:39:28 +0100

    removed unused fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Bebas_Neue, Nunito } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
@@ -20,17 +19,6 @@ export const metadata: Metadata = {
     },
 };
 
-const bebas = Bebas_Neue({
-    subsets: ["latin"],
-    weight: "400",
-    variable: "--font-bebas",
-});
-const nunito = Nunito({
-    subsets: ["latin"],
-    weight: ["200", "300", "400", "500", "600", "700", "800", "900", "1000"],
-    variable: "--font-nunito",
-});
-
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -39,7 +27,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`min-h-screen antialiased bg-primary text-secondary font-nunito ${nunito.variable} ${bebas.variable}`}
+                className={`min-h-screen antialiased bg-primary text-secondary`}
             >
                 <Header />
                 <main>{children}</main>
@@ -48,3 +36,19 @@ export default function RootLayout({
         </html>
     );
 }
+
+/*
+# Notes
+
+- minimal header with animate down menu
+- scale down projects list, animate scale on scroll,
+- list in the center of the page, make it only half the width of the page to have a natural border around the egde
+- project info shows up on the image and links to project popup
+- project popup has a small description, images and links to project and github repo
+- collabs section is an accordian with a title, description and link to project
+- contact is direct to email
+- the only additional page is the info/about page
+- the about page will have information about me, teck stack and anything extra i can think of
+- all pages share header, footer
+
+*/
