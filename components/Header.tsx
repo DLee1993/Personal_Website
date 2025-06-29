dc5fdd143bd0b89b4605f8094cc2016831a8678a
David Lee <Dai180293@gmail.com>
2025-06-29 09:40:06 +0100

    added menu component to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import Menu from "@/components/Menu";
+
 export default function Header() {
     return (
         <header>
             header
+            <Menu />
         </header>
     );
 }
