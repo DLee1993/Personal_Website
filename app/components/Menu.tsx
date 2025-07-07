3aad1d0f2147ddc57e2b55a724fcedcea45c9f37
David Lee <Dai180293@gmail.com>
2025-07-07 18:55:25 +0100

    added button to menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,5 +1,12 @@
 export default function Menu() {
-  return (
-    <div>Menu</div>
-  )
+    return (
+        <>
+            {/* Menu button */}
+            <button className="relative space-y-1 cursor-pointer w-[40px] h-[20px] p-1">
+                <span className="absolute block w-full h-[1.5px] top-2 left-0 bg-dark"></span>
+                <span className="absolute block w-full h-[1.5px] top-3 left-0 bg-dark"></span>
+            </button>
+            {/* Menu content */}
+        </>
+    );
 }
