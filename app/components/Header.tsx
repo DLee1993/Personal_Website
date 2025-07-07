5678b0fda44bd9d9f5cc658e27c073f61cda8950
David Lee <Dai180293@gmail.com>
2025-07-07 18:55:14 +0100

    added all content to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,10 @@
+import Menu from "@/app/components/Menu";
+
 export default function Header() {
-    return <header>header</header>;
+    return (
+        <header className="sticky top-0 left-0 p-7 flex justify-between items-center">
+            <p className="font-semibold">David Lee</p>
+            <Menu />
+        </header>
+    );
 }
