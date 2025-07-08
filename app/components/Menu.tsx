d752fa0a7cde4cf1a7c9b0dae3e5e7849ccdafac
David Lee <Dai180293@gmail.com>
2025-07-08 17:43:55 +0100

    added menu file

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -2,7 +2,7 @@ export default function Menu() {
     return (
         <>
             {/* Menu button */}
-            <button className="relative space-y-1 cursor-pointer w-[40px] h-[20px] p-1">
+            <button className="relative space-y-1 cursor-pointer w-[30px] h-[20px] p-1">
                 <span className="absolute block w-full h-[1.5px] top-2 left-0 bg-dark"></span>
                 <span className="absolute block w-full h-[1.5px] top-3 left-0 bg-dark"></span>
             </button>
