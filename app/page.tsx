c5020596d1b61254da5a6488b67c045e5feb0966
David Lee <Dai180293@gmail.com>
2025-07-08 18:10:45 +0100

    corrected typo's

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <section>
             <h1 className="text-base font-medium max-w-2xl my-20">
-                A a React-based developer focused on experiences that go beyond aesthetics—building
+                A React-based developer focused on experiences that go beyond aesthetics—building
                 websites that serve, inform, and engage. I code with the intention to create
                 meaningful, lasting impact.
             </h1>
