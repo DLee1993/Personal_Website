fc3e148aed014706a216e2ceacd457687c73b244
David Lee <Dai180293@gmail.com>
2025-07-08 18:03:00 +0100

    reduced font size

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ProjectsList from "@/app/components/ProjectsList";
 export default function Home() {
     return (
         <section>
-            <h1 className="text-lg max-w-2xl my-20">
+            <h1 className="text-base font-medium max-w-2xl my-20">
                 A a React-based developer focused on experiences that go beyond aesthetics—building
                 websites that serve, inform, and engage. I code with the intention to create
                 meaningful, lasting impact.
