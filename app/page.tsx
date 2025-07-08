818856af80c938e81dc145249db3dc3c5586a148
David Lee <Dai180293@gmail.com>
2025-07-08 17:44:18 +0100

    added root text and projects component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import ProjectsList from "@/app/components/ProjectsList";
+
 export default function Home() {
-    return <>hello</>;
+    return (
+        <section>
+            <h1 className="text-md md:text-lg max-w-xl my-20">
+                A a React-based developer focused on experiences that go beyond aesthetics—building
+                websites that serve, inform, and engage. I code with the intention to create
+                meaningful, lasting impact.
+            </h1>
+            <ProjectsList />
+        </section>
+    );
 }
