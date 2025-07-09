8f6d2906e4c91e1feb7cf74982e9d9c25d3e042e
David Lee <Dai180293@gmail.com>
2025-07-09 20:46:46 +0100

    added a section header component for reusablility

---
+++ app/components/SectionHeader.tsx
@@ -0,0 +1,8 @@
+export default function SectionHeader({ text }: { text: string }) {
+    return (
+        <div className="my-10 flex gap-2">
+            <div className="size-1 mt-1 bg-dark"></div>
+            <h2 className="capitalize text-dark/50">{text}</h2>
+        </div>
+    );
+}
