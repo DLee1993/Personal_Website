29c3a49d0f5ebe9c0ea04da5b2e6fa7d3e1ffafc
David Lee <Dai180293@gmail.com>
2025-07-10 05:54:16 +0100

    fixed margin issue pushing element up the screen

--- app/components/ProjectView.tsx
+++ app/components/ProjectView.tsx
@@ -2,7 +2,7 @@ import { Project } from "@/app/components/ProjectsList";
 
 export default function ProjectView({ project }: { project: Project }) {
     return (
-        <div className="fixed bottom-0 left-0 z-50 bg-dark text-light w-full h-2/3">
+        <div className="fixed bottom-0 left-0 z-50 bg-dark text-light w-full h-2/3 m-0">
             <h3>{project.title}</h3>
             <p>{project.description}</p>
             {/* You can animate in more details with Framer Motion or GSAP here */}
