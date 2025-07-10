2c66111cfb734c432c1254f61a9624a6b9bf423e
David Lee <Dai180293@gmail.com>
2025-07-10 05:17:28 +0100

    added a margin bottom

--- app/components/SectionHeader.tsx
+++ app/components/SectionHeader.tsx
@@ -1,6 +1,6 @@
 export default function SectionHeader({ text }: { text: string }) {
     return (
-        <div className="flex gap-2">
+        <div className="flex gap-2 mb-5">
             <div className="size-1 mt-1 bg-dark"></div>
             <h2 className="capitalize text-dark/50">{text}</h2>
         </div>
