110a9ba42bab52a1913cf4e82fd4ef36096aa6e8
David Lee <Dai180293@gmail.com>
2025-07-10 02:51:14 +0100

    updated text content

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <header className="sticky top-0 left-0 h-16 flex justify-between items-center bg-light z-50">
             <p>David Lee</p>
             <div className="flex justify-between space-x-10 sm:space-x-16 md:space-x-20">
-                <p className="text-dark/50 hidden sm:block">Cardiff, United Kingdom</p>
+                <p className="text-dark/50 hidden sm:block">Available for work</p>
                 <ul className="hidden min-[400px]:flex gap-5">
                     {socialLinks.map((l, i) => (
                         <li key={i}>
