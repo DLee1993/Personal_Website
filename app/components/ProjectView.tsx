1215ccd1d5d8e584303a56b91426042c12a4a43c
David Lee <Dai180293@gmail.com>
2025-07-18 00:12:11 +0100

    added additional headings to describe the content below it, added additional styling for the external links

--- app/components/ProjectView.tsx
+++ app/components/ProjectView.tsx
@@ -24,6 +24,9 @@ export default function ProjectView({
         gsap.to("#popup", {
             y: open ? 0 : "100%",
         });
+        gsap.to(document.body, {
+            overflowY: open ? "hidden" : "auto",
+        });
     }, [open]);
 
     return (
@@ -39,25 +42,40 @@ export default function ProjectView({
                     <span className="inline-block rotate-45">+</span>
                 </button>
             </nav>
-            <section className="w-full h-full flex flex-col md:flex-row justify-between items-start p-5 overflow-y-scroll gap-10 md:gap-5">
+            <section className="w-full h-full flex flex-col md:flex-row justify-between items-start px-2 sm:px-4 md:px-5 overflow-y-scroll gap-10">
                 <article className="md:max-w-1/2 md:sticky top-0 space-y-10">
                     <div>
-                        <sup>{project?.type}</sup>
-                        <h3 className="font-semibold text-lg">{project?.title}</h3>
+                        <sup className="text-xs text-light/50">{project?.type}</sup>
+                        <h3 className="font-semibold text-xl">{project?.title}</h3>
                     </div>
-                    <p className="text-light/75">{project?.description}</p>
+                    <div className="space-y-2">
+                        <p className="text-xs text-light/50">Description</p>
+                        <p className="text-xs sm:text-sm">{project?.description}</p>
+                    </div>
+
+                    <div className="space-y-2">
+                        <p className="text-xs text-light/50">Tech stack</p>
+                        <ul className="flex flex-wrap gap-2 capitalize text-xs sm:text-sm">
+                            {project?.techStack.map((t, i) => (
+                                <li key={i}>{t}</li>
+                            ))}
+                        </ul>
+                    </div>
                     <div className="space-x-10">
-                        <Link href={project ? project.repository : ""} className="customUnderlineLight">
-                            View the code
-                        </Link>
-                        <Link href={project ? project.url : ""} className="customUnderlineLight">
+                        <Link
+                            href={project ? project.url : ""}
+                            className="border border-light px-4 py-2 text-xs sm:text-sm rounded-md hover:text-dark hover:bg-light"
+                        >
                             Visit the site
+                        </Link>
+                        <Link href={project ? project.repository : ""} className="customUnderlineLight text-xs sm:text-sm">
+                            View the code
                         </Link>
                     </div>
                 </article>
-                <figure className="space-y-5 columns-2 md:columns-1 flex flex-col justify-center items-center">
+                <figure className="space-y-5 columns-2">
                     {project?.images.map((img, i) => (
-                        <img src={img.src} alt={img.caption} key={i} className="w-10/12" />
+                        <img src={img.src} alt={img.caption} key={i} />
                     ))}
                 </figure>
             </section>
