6b892f4309521665174c4cd4e311407979e318bb
David Lee <Dai180293@gmail.com>
2025-07-18 00:10:27 +0100

    added additional spacing for right side elements on tablet viewports

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,9 +6,9 @@ export default function Header() {
         <header className="sticky top-0 left-0 h-16 flex justify-between items-center bg-light z-50">
             <p>David Lee</p>
             <div className="flex justify-between space-x-10 sm:space-x-16 md:space-x-40">
-                <div className="flex justify-between md:space-x-14">
+                <div className="flex justify-between min-[500px]:space-x-8 md:space-x-14">
                     <p className="text-dark/50 block">Cardiff, United Kingdom</p>
-                    <ul className="hidden min-[400px]:flex gap-5">
+                    <ul className="hidden min-[500px]:flex gap-5">
                         {socialLinks.map((l, i) => (
                             <li key={i}>
                                 <Link href={l.src} target="_blank" className="customUnderlineDark">
