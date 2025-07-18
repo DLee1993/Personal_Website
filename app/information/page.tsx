0c6f04d317eb394662dd520ada05693af655e9c8
David Lee <Dai180293@gmail.com>
2025-07-18 23:19:17 +0100

    added all page content

--- app/information/page.tsx
+++ app/information/page.tsx
@@ -1,3 +1,37 @@
+import personal from "@/data/personal.json";
+
 export default function Information() {
-    return <div>Information page</div>;
+    return (
+        <section className="my-16 md:my-20 space-y-20 md:space-y-28 lg:space-y-32">
+            <section className="flex flex-wrap justify-between items-end gap-10">
+                <article className="space-y-5">
+                    <h1 className="text-base space-y-5 max-w-3xl">
+                        <p>
+                            Continuous learning is the bedrock of my personal and professional
+                            development. By tackling hands-on projects, specialized courses, and
+                            collaborative challenges, I&apos;ve expanded my skills and reached new
+                            milestones. Outside the digital realm, I test my mental and physical
+                            limits at a kickboxing academy—training rigorously and competing in
+                            tournaments.
+                        </p>
+                        <p>
+                            Now I&apos;m ready to launch my career in tech, eager to embrace
+                            challenges, drive innovation, and develop solutions with real impact.
+                        </p>
+                    </h1>
+                </article>
+                <p className="customUnderlineDark cursor-pointer">View Resume</p>
+            </section>
+            <section className="flex flex-wrap gap-10 sm:gap-20 md:gap-32 lg:gap-40">
+                {personal.map((p, i) => (
+                    <ul key={i}>
+                        <li className="text-xs text-dark/75 mb-3">{p.listHeader}</li>
+                        {p.listItems.map((item, i) => (
+                            <li key={i}>{item}</li>
+                        ))}
+                    </ul>
+                ))}
+            </section>
+        </section>
+    );
 }
