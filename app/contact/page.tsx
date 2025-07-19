e871cf8ac8ec8d0775811ceb2eed400b39294ee1
David Lee <Dai180293@gmail.com>
2025-07-19 05:31:22 +0100

    added thank you message

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,12 @@
 export default function Contact() {
     return (
-        <section className="my-16 md:my-20 space-y-20 md:space-y-28 lg:space-y-32">
+        <section className="my-16 md:my-20 space-y-20">
+            <h1 className="text-base space-y-5 max-w-3xl">
+                Thanks for taking the time to explore my portfolio. If you&apos;d like to
+                collaborate, chat about a potential project, or simply learn more about what I do,
+                feel free to fill out the form below. I&apos;d love to hear from you and see where
+                our ideas could take us!
+            </h1>
             <form className="space-y-5">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-2">
                     <fieldset>
