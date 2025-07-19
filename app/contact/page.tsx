1b4ec168691e5f2caa44a74544e2f8f26f1fe7c8
David Lee <Dai180293@gmail.com>
2025-07-19 05:36:06 +0100

    adjusted y axis spacing

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <section className="my-16 md:my-20 space-y-20">
+        <section className="my-16 md:my-20 space-y-16 sm:space-y-20">
             <h1 className="text-base space-y-5 max-w-3xl">
                 Thanks for taking the time to explore my portfolio. If you&apos;d like to
                 collaborate, chat about a potential project, or simply learn more about what I do,
